6b5def8b2998d5fab7568961d4c4b54f6859fc2f
Oxxion <156255933+Oxxion@users.noreply.github.com>
2024-01-15 16:48:57 -0800

    Update download.ps1

--- github/workflows/download.ps1
+++ github/workflows/download.ps1
@@ -1,10 +1,11 @@
 # Definir la URL del instalador de Adobe Acrobat Reader en GitHub
-$url = 'https://raw.githubusercontent.com/Oxxion/Archivos/main/github/workflows/download.ps1'
+$url = 'https://github.com/Oxxion/Archivos/raw/main/Reader_Install_Setup.exe'
 
 # Definir la ruta donde se guardará el instalador en el escritorio
-$installerPath = [System.IO.Path]::Combine($env:USERPROFILE, 'Desktop\AdobeAcrobatReaderInstaller.exe')
+$installerPath = [System.IO.Path]::Combine($env:USERPROFILE, 'Desktop\Reader_Install_Setup.exe')
 
-# Descargar e instalar Adobe Acrobat Reader
+# Descargar Adobe Acrobat Reader
 Invoke-WebRequest -Uri $url -OutFile $installerPath
-Start-Process -FilePath $installerPath -ArgumentList "/sAll /rs /msi"
 
+# Ejecutar el instalador (agregando /SILENT para la instalación silenciosa)
+Start-Process -FilePath $installerPath -ArgumentList "/SILENT"
