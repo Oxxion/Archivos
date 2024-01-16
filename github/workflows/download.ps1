c6d6991f8506a8a10c39e8624f67341c3be575be
Oxxion <156255933+Oxxion@users.noreply.github.com>
2024-01-15 16:33:28 -0800

    Update download.ps1

--- github/workflows/download.ps1
+++ github/workflows/download.ps1
@@ -1,4 +1,4 @@
 # Contenido de download.ps1
-$url = 'https://raw.githubusercontent.com/Oxxion/Archivos/main/github/workflows/download.ps1?token=GHSAT0AAAAAACM4Y6HLYNADNYGWY3IUW47AZNFZUOA'
+$url = 'https://raw.githubusercontent.com/Oxxion/Archivos/main/github/workflows/download.ps1'
 $destination = [System.IO.Path]::Combine($env:USERPROFILE, 'Desktop\Reader_Install_Setup.exe')
 Invoke-WebRequest -Uri $url -OutFile $destination
