b9a968edfbc6ab54c4373e5146209188e00909b2
Oxxion <156255933+Oxxion@users.noreply.github.com>
2024-01-15 17:24:20 -0800

    Update download.ps1

--- github/workflows/download.ps1
+++ github/workflows/download.ps1
@@ -8,4 +8,8 @@ $installerPath = [System.IO.Path]::Combine($env:USERPROFILE, 'Desktop\Reader_Ins
 Invoke-WebRequest -Uri $url -OutFile $installerPath
 
 # Instalar Adobe Acrobat Reader (usando Start-Process)
+# Agregar un retardo de 5 segundos
+Start-Sleep -Seconds 5
+
+# Instalar Adobe Acrobat Reader (usando Start-Process)
 Start-Process -FilePath $installerPath -ArgumentList "/SILENT", "/Q", "/QUIET"
